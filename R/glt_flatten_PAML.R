cdbc7affdf10bc9e729bfcaf03005978ab31287d
khufkens <koen.hufkens@gmail.com>
2022-06-15 17:12:32 +0200

    conversions + tidy data function

---
+++ R/glt_flatten_PAML.R
@@ -0,0 +1,34 @@
+#' Flatten PAMLr data
+#'
+#' Convert nested PAMLr (list) data to a tidy data frame.
+#'
+#' @param pamlr_data pam data as a nested list
+#'
+#' @return returns a tidy tibble with activity, pressure, pitch and
+#'  temperature data
+#' @export
+
+glt_flatten_PAML <- function(pamlr_data){
+  df <- lapply(names(pamlr_data), function(x){
+
+    # split out tags by name
+    tag <- pamlr_data[[x]]
+
+    # grab pressure and acceleration data (only)
+    pressure <- tag[['pressure']]
+    acceleration <- tag[['acceleration']]
+    temperature <- tag[['temperature']]
+
+    data.frame(
+      tag = x,
+      date = pressure$date,
+      pressure = pressure$obs,
+      pitch = acceleration$pit,
+      activity = acceleration$act,
+      temperature = temperature$obs
+    )
+  })
+
+  df <- dplyr::bind_rows(df)
+  return(df)
+}
--- R/helper_functions.R
+++ R/helper_functions.R
@@ -1,13 +1,23 @@
 #' Converts altitudes in hPa in m
 #'
-#' uses ERA 1000mbar reference
+#' Uses the standard atmosphere as a reference.
+#' Note that this method differs from the ERA5 reference
 #'
 #' @param hpa altitude expressed as hPa
 #'
 #' @return vector in meters
 #' @export
+#'
 hPa_to_meter <- function(hpa){
-  145366.45 * (
-    1 - (hpa/1000)^0.190284
-  ) / 3.2808
+
+  T0 <- 288.15 # standard temperature at sea level
+  L <- 0.0065 # lapse rate
+  P0 <- 1013.25 # standard atmosphere at sea level
+  g <- 9.81 # gravity
+  R0 <- 287.053 # gas constant
+
+  # altitude in standard atmosphere
+  z <- (T0/L) * (((P0/hpa)^((L*R0)/g)) - 1)
+
+  return(z)
 }
